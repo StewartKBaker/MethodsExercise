86c10e53a4a94181b5f826614559858e0618feb6
StewartBaker <bakrstewart16@gmail.com>
2024-07-09 12:05:00 -0500

    Exercise One

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,31 @@
 {
     public class Program
     {
+        //Exercise One
+        public static void ShortStory()
+        {
+            Console.WriteLine("What is your Name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("What is your Favorite Color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite Video Game?");
+            string favGame = Console.ReadLine();
+            Console.WriteLine("What is your Favorite Animal?");
+            string favAnimal = Console.ReadLine();
+            Console.Clear(); 
+            
+            Console.WriteLine($"One Day, {userName} was walking down the street. {userName} saw a {favAnimal} wearing {favColor} pants, playing {favGame}! How crazy is that!!Ste");
+            Console.WriteLine("Ready to clear?");
+            Console.ReadLine();
+            Console.Clear();
+        }
+ 
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise One
+            ShortStory();
+
         }
     }
 }
